e083178ff3b78a67e4ca5d35bfac2fc7e43c1dc9
aerth <aerth@isupon.us>
2015-07-26 20:35:08 -0700

    Fix Doobie.

--- cannabis.lua
+++ cannabis.lua
@@ -80,9 +80,8 @@ minetest.register_craft({
 	recipe = {"farming_cannabis:cannabis"},
 })
 minetest.register_craftitem("farming_cannabis:cannabis_doobie", {
-description = "Cannabis Doobie",
+description = "Cannabis Doobie :D",
 image = "farming_cannabis_doobie.png",
-description = "Cannabis Bud",
 on_place = minetest.item_place,
 on_use = minetest.item_eat(1)
 })
